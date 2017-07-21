3aab5bedb13153e1af4b60843f496c30cd67e145
tripeno <victor.tripeno@gft.com>
2017-07-21 18:19:19 -0300

    RestRoute Creation

---
+++ src/main/java/br/com/code/sorcerers/routes/RestRoute.java
@@ -0,0 +1,12 @@
+package br.com.code.sorcerers.routes;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class RestRoute extends RouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		
+	}
+
+}

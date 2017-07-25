7842c03f95762d4d327af8a7706700e57946515b
tripeno <victor.tripeno@gft.com>
2017-07-25 15:24:24 -0300

    Hystrix

--- src/main/java/br/com/code/sorcerers/routes/RestRoute.java
+++ src/main/java/br/com/code/sorcerers/routes/RestRoute.java
@@ -1,5 +1,9 @@
 package br.com.code.sorcerers.routes;
 
+import java.io.IOException;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 
@@ -7,6 +11,17 @@ public class RestRoute extends RouteBuilder {
 
 	@Override
 	public void configure() throws Exception {
+		
+		// Pega a as URIS e separa em um array dividido por virgula
+		String[] uris = getContext().resolvePropertyPlaceholders("{{addresses}}").split(",");
+		String[] nettyList = new String[uris.length];
+		
+		// Itera o array de URIS para poder montar os caminhos desejados de socket
+		for(int i = 0; i < uris.length; i++) {
+			StringBuilder path = new StringBuilder("netty4-http:http://");
+			path.append(uris[i]);//.append("?textline=true&synchronous=").append(String.valueOf(sync)).append("&clientMode=true");
+			nettyList[i] = path.toString();
+		}
 		
 		restConfiguration().component("spark-rest")
 			// configuracao de contexto, host e porta
@@ -24,9 +39,22 @@ public class RestRoute extends RouteBuilder {
 			.post("/socket")
 		.toD("direct:teste");
 		
-		from("direct:teste")
-			.setBody(simple("TESTE BODY"))
-			.to("log:foo")
+		from("seda:distribuidor")
+			.log("${body}")
+			.process(new Processor() {
+				
+				@Override
+				public void process(Exchange exchange) throws Exception {
+					System.out.println("BODY " + exchange.getIn().getBody());
+				}
+			})
+			.loadBalance()
+			.hystrix()
+			// TODO
+			
+			
+			
+			.inOut(nettyList)
 		.end();
 	}
 

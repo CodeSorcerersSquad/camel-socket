0019471e0b7aa8c2b787f7e616fce54fab20ffd7
tripeno <victor.tripeno@gft.com>
2017-08-01 11:45:53 -0300

    Spring boot

--- src/main/java/br/com/code/sorcerers/routes/RestRoute.java
+++ src/main/java/br/com/code/sorcerers/routes/RestRoute.java
@@ -1,5 +1,8 @@
 package br.com.code.sorcerers.routes;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -9,11 +12,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class RestRoute extends RouteBuilder {
 
+
+	private String host;
+	private String port;
+	private String addresses;
+	private Boolean sync;
+	Properties property = new Properties();
+	
 	@Override
 	public void configure() throws Exception {
 		
+		property.load(new FileInputStream("src/main/resources/socket.properties")); 
+		this.host = property.getProperty("host");
+		this.port = property.getProperty("port");
+		this.addresses = property.getProperty("addresses");
+		this.sync = Boolean.valueOf(property.getProperty("sync"));
+		
+		
 		// Pega a as URIS e separa em um array dividido por virgula
-		String[] uris = getContext().resolvePropertyPlaceholders("{{addresses}}").split(",");
+		String[] uris = getContext().resolvePropertyPlaceholders(addresses).split(",");
 		String[] nettyList = new String[uris.length];
 		
 		// Itera o array de URIS para poder montar os caminhos desejados de socket
@@ -25,7 +42,7 @@ public class RestRoute extends RouteBuilder {
 		
 		restConfiguration().component("spark-rest")
 			// configuracao de contexto, host e porta
-			.contextPath("/").host("{{host}}").port("{{port}}")
+			.contextPath("/").host(host).port(port)
 			// configuracao de binding para efetuar automaticamente bind
 			// json para pojo
 			.bindingMode(RestBindingMode.json)
@@ -54,11 +71,12 @@ public class RestRoute extends RouteBuilder {
 		            .executionTimeoutInMilliseconds(5000).circuitBreakerSleepWindowInMilliseconds(10000)
 		       .end()
 //		       .inOut(nettyList)
+		       .log("${body}")
 			.onFallback()
 				.setBody(simple("ERROR!!"))
 			.end()
 			
-			.log("${body}")
+			.to("log:foo")
 		.end();
 	}
 
--- src/main/java/br/com/code/sorcerers/routes/ServiceApplication.java
+++ src/main/java/br/com/code/sorcerers/routes/ServiceApplication.java
@@ -1,41 +1,42 @@
 package br.com.code.sorcerers.routes;
 
+//import org.apache.camel.component.servlet.CamelHttpTransportServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-/*
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.web.filter.CharacterEncodingFilter;
-*/
+//import org.springframework.web.filter.CharacterEncodingFilter;
+
 
 @SpringBootApplication
+@ImportResource({ "classpath:META-INF/spring/camel-context.xml" })
 public class ServiceApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		SpringApplication.run(RestRoute.class, args);
+		SpringApplication.run(ServiceApplication.class, args);
 	}
 	
-	/*
-	@Bean
-	ServletRegistrationBean servletRegistrationBean() {
-		ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/rest/*");
-		servlet.setName("CamelServlet");
-		return servlet;
-	}
-
-	@Bean
-	@Order(Ordered.HIGHEST_PRECEDENCE)
-	CharacterEncodingFilter characterEncodingFilter() {
-		CharacterEncodingFilter filter = new CharacterEncodingFilter();
-		filter.setEncoding("UTF-8");
-		filter.setForceEncoding(true);
-
-		return filter;
-	}
-	*/
+	
+//	@Bean
+//	ServletRegistrationBean servletRegistrationBean() {
+//		ServletRegistrationBean servlet = new ServletRegistrationBean(new CamelHttpTransportServlet(), "/rest/*");
+//		servlet.setName("CamelServlet");
+//		return servlet;
+//	}
+//
+//	@Bean
+//	@Order(Ordered.HIGHEST_PRECEDENCE)
+//	CharacterEncodingFilter characterEncodingFilter() {
+//		CharacterEncodingFilter filter = new CharacterEncodingFilter();
+//		filter.setEncoding("UTF-8");
+//		filter.setForceEncoding(true);
+//
+//		return filter;
+//	}
+	
 
 }

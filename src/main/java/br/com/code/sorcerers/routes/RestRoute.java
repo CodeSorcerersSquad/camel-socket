21c2bddd1792b6966c6afece9fd877e33b26181c
tripeno <victor.tripeno@gft.com>
2017-08-03 18:00:07 -0300

    Properties Adjust

--- src/main/java/br/com/code/sorcerers/routes/RestRoute.java
+++ src/main/java/br/com/code/sorcerers/routes/RestRoute.java
@@ -1,8 +1,5 @@
 package br.com.code.sorcerers.routes;
 
-import java.io.FileInputStream;
-import java.util.Properties;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
@@ -11,26 +8,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class RestRoute extends RouteBuilder {
-
-
-	private String host;
-	private String port;
-	private String addresses;
-	private Boolean sync;
-	Properties property = new Properties();
 	
 	@Override
 	public void configure() throws Exception {
 		
-		property.load(new FileInputStream("src/main/resources/socket.properties")); 
-		this.host = property.getProperty("host");
-		this.port = property.getProperty("port");
-		this.addresses = property.getProperty("addresses");
-		this.sync = Boolean.valueOf(property.getProperty("sync"));
-		
-		
 		// Pega a as URIS e separa em um array dividido por virgula
-		String[] uris = getContext().resolvePropertyPlaceholders(addresses).split(",");
+		String[] uris = getContext().resolvePropertyPlaceholders("{{addresses}}").split(",");
 		String[] nettyList = new String[uris.length];
 		
 		// Itera o array de URIS para poder montar os caminhos desejados de socket
@@ -38,11 +21,12 @@ public class RestRoute extends RouteBuilder {
 			StringBuilder path = new StringBuilder("netty4-http:http://");
 			path.append(uris[i]);
 			nettyList[i] = path.toString();
+			System.out.println(nettyList[i]);
 		}
 		
 		restConfiguration().component("spark-rest")
 			// configuracao de contexto, host e porta
-			.contextPath("/").host(host).port(port)
+			.contextPath("/").host("{{host}}").port("{{port}}")
 			// configuracao de binding para efetuar automaticamente bind
 			// json para pojo
 			.bindingMode(RestBindingMode.json)
@@ -67,7 +51,6 @@ public class RestRoute extends RouteBuilder {
 					System.out.println("BODY " + exchange.getIn().getBody());
 				}
 			})
-//			.loadBalance()
 			.hystrix()
 				.hystrixConfiguration()
 		            .executionTimeoutInMilliseconds(5000).circuitBreakerSleepWindowInMilliseconds(10000)
